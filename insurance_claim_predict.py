14afb427c1787d5befc700671fb7d1d7ed25d48a
Xuqing Wu <92066319+xw88-1933202@users.noreply.github.com>
2023-02-20 01:04:18 -0800

    Update insurance_claim_predict.py

--- insurance_claim_predict.py
+++ insurance_claim_predict.py
@@ -12,8 +12,23 @@ def filter_file(df: pd.DataFrame):
     Drop uninformative rows.
     '''
     df = df.dropna().reset_index(drop=True)          # drop all rows with NaN values
-    
+    rows_count = len(df.index)
 
+    
+def fit_and_predict_degrees(df: pd.DataFrame) -> float:
+    '''
+    Build the machine learning model and reture the MSE.
+    '''
+    df = df[['age', 'gender', 'bmi', 'bloodpressure', 'diabetic', 'children', 'smoker',	'region', 'claim']]
+    features = df.loc[:, df.columns != 'claim']
+    labels = df['claim']
+    features = pd.get_dummies(features)
+    features_train, features_test, labels_train, labels_test = \
+        train_test_split(features, labels, test_size=0.2, random_state=2)
+    model = DecisionTreeRegressor()
+    model.fit(features_train, labels_train)
+    predictions = model.predict(features_test)
+    return mean_squared_error(labels_test, predictions)
    
 
 
@@ -25,6 +40,7 @@ def main():
     data = pd.read_csv('insurance_data.csv', na_values=['---'])
     # Call functions here
     filter_file(data)
+    fit_and_predict_degrees(data)
 
 
 if __name__ == '__main__':
