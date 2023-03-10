57d27c730e01ce123d955518bb2adea22bb0026e
mayuzhuo-2062601 <102702263+mayuzhuo-2062601@users.noreply.github.com>
2023-03-09 18:49:23 -0800

    Revert "modify the diabetic and test_file"
    This reverts commit 10bb2f7fceab6e5d50bacf8f1cfe2fd00933d8f8.

--- test_file.py
+++ test_file.py
@@ -1,31 +1,31 @@
 import pandas as pd
-from diabetic import filter_file, fit_and_predict_diabetic, hyperparameter_tuning
+import diabetic
 
 # Test data
-def test_filter_file(data):
+def test_filter_file():
     # Test if function returns a DataFrame with the expected columns
     result = filter_file(data)
-    assert all(col in result.columns for col in ['age', 'gender', 'bmi', 'bloodpressure', 'diabetic', 'children', 'smoker',	'region', 'claim'])
+    assert all(col in result.columns for col in ['age', 'gender', 'bmi', 'bloodpressure', 'diabetic', 'smoker', 'claim'])
 
-def test_fit_and_predict_diabetic(data):
+def test_fit_and_predict_diabetic():
     # Test if function returns a float between 0 and 1
     result = fit_and_predict_diabetic(data)
     assert isinstance(result, float)
     assert 0 <= result <= 1
 
-def test_hyperparameter_tuning(data):
+def test_hyperparameter_tuning():
     # Test if function returns a DataFrame with the expected columns
     result = hyperparameter_tuning(data)
     assert all(col in result.columns for col in ['Accuracy', 'P_Criterion', 'P_Splitter', 'P_MaxDepth'])
 
 
 def main():
-    data = pd.read_csv('insurance_data.csv')
-    data = filter_file(data)
-    test_filter_file(data)
-    test_fit_and_predict_diabetic(data)
-    test_hyperparameter_tuning(data)
-
+    df = pd.read_csv('insurance_data.csv')
+    test_df = filter_file(df)
+    test_filter_file(test_df)
+    test_fit_and_predict_diabetic(test_df)
+    test_hyperparameter_tuning(test_df)
+    
 
 if __name__ == '__main__':
     main()
