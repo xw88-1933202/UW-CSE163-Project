f0d5815a7ed8fe290bc4a998f229908dd9e4fcc9
mayuzhuo-2062601 <102702263+mayuzhuo-2062601@users.noreply.github.com>
2023-03-10 22:43:33 -0800

    Create scatter plot.py

---
+++ scatter plot.py
@@ -0,0 +1,37 @@
+import pandas as pd
+import plotly.express as px
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+
+# Load the dataset
+df = pd.read_csv('insurance_data.csv')
+
+# Define the app layout
+app = dash.Dash(__name__)
+app.layout = html.Div([
+    dcc.Graph(id='scatter-plot',
+              figure=px.scatter(df, x='bmi', y='claim', color='gender', trendline='ols'))
+])
+
+@app.callback(
+    dash.dependencies.Output('scatter-plot', 'figure'),
+    [dash.dependencies.Input('gender-dropdown', 'value')])
+def update_figure(selected_gender):
+    filtered_df = df[df['gender'] == selected_gender]
+    fig = px.scatter(filtered_df, x='bmi', y='claim', color='gender', trendline='ols')
+    fig.update_traces(marker=dict(
+        color=['blue' if gender == 'male' else 'red' for gender in df['gender']]
+    ))
+    return fig
+
+# Add the dropdown menu to the app layout
+app.layout = html.Div([
+    dcc.Dropdown(id='gender-dropdown', options=[{'label': 'Male', 'value': 'male'},
+                                                 {'label': 'Female', 'value': 'female'}],
+                 value='male'),
+    dcc.Graph(id='scatter-plot', figure=px.scatter(df, x='bmi', y='claim', color='gender', trendline='ols'))
+])
+# Run the app
+if __name__ == '__main__':
+    app.run_server(debug=True)
